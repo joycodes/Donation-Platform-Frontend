b42c6bd05aac312044515ddb262b4964899d2b9b
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-07-10 16:08:01 +0300

    ft:modified services  and api consuption

--- src/app/services/donate.service.ts
+++ src/app/services/donate.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DonateserviceService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  // Providing the charities api
+  charitiesApiUrl = 'https://donationapp254.herokuapp.com/api/charities/';
+
+  // Providing the charity by id api
+
+  // charityByIdUrl = 'https://donationapp254.herokuapp.com/api/charities/charities-id/4'
+
+
+
+  // Start of getting the specific charity
+  getCharity(id:any) {
+    let charityUrl = 'https://donationapp254.herokuapp.com/api/charities/charities-id/' + id;
+
+    return this._http.get<any>(charityUrl)
+  }
+  // End of charity by id call
+
+
+
+  // Creating a function to grab the objects from the api
+  charitiesProps():Observable<any> {
+    return this._http.get(this.charitiesApiUrl)
+  }
+
+
+  // Getting details of a single id
+  // charityProp():Observable<any> {
+  //   return this._http.get(this.charityByIdUrl)
+  // }
+
+  // Posting donations
+  donationsUrl = 'https://donationappke.herokuapp.com/api/donations/';
+
+  donationsMade(userData:any):Observable<any> {
+    return this._http.post(this.donationsUrl, userData);
+  }
+
 }
+
