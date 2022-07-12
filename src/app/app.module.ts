6e3b4177abad4fc67cae4c49160469a726fa5f44
joycodes <jnguttu@gmail.com>
2022-07-12 13:52:44 +0300

    testing end points

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
 import { HomeviewComponent } from './homeview/homeview.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { CharitiesComponent } from './charities/charities.component';
+import { CharitiesDetailsComponent } from './charities-details/charities-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeviewComponent,
     FooterComponent,
-    HomeviewComponent
+    AboutComponent,
+    CharitiesComponent,
+    CharitiesDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
 
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,11 @@ export const environment = {
   production: false
 };
 
+// export const BASEURL = `https://donationappke.herokuapp.com/api`
+
+export const BASEURL = `http://127.0.0.1:8000/api`
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
